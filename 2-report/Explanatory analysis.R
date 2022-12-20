494fa3195dbbd019ae1f57b9755aedadf610a086
MantasKas <119167556+MantasKas@users.noreply.github.com>
2022-12-20 22:39:16 +0200

    Add files via upload

--- 2-report/Explanatory analysis.R
+++ 2-report/Explanatory analysis.R
@@ -4,21 +4,37 @@
 
 library(tidyverse)
 library(knitr)
-h2o.init()
 
-df <- h2o.importFile("C:/Users/Mantas/Downloads/train_data.csv")
+df2 <- read_csv("C:/Users/Mantas/Downloads/train_data.csv")
 
-dim(df)
+dim(df2)
 
-summary(df[1:6])
+summary(df2[1:6])
 
-summary(df[7:13]) %>%
+
+summary(df2[7:13]) %>%
   kable()
 
-df$loan_purpose <- as.factor(df$loan_purpose)
-df$y <- as.factor(df$y)
+df2$loan_purpose <- as.factor(df2$loan_purpose)
+df2$y <- as.factor(df2$y)
 
-summary(df$loan_purpose) %>%
+summary(df2$loan_purpose) %>%
   kable()
 
-h2o.shutdown()
+df2 %>%
+  group_by(loan_purpose) %>%
+  summarise(n = n()) %>%
+  ggplot(aes(fill=y, y=n, x=loan_purpose)) + 
+  geom_bar(position="dodge", stat="identity") + 
+  coord_flip() +
+  scale_y_continuous(labels = scales::comma) +
+  theme_dark()
+
+df2 %>%
+  group_by(loand_purpose) %>%
+  summarise(n = n()) %>%
+  ggplot(aes(fill=y, y=n, x=credit_score)) + 
+  geom_bar(position="dodge", stat="identity") + 
+  coord_flip() +
+  scale_y_continuous(labels = scales::comma) +
+  theme_dark()
